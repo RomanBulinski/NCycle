8cd7956a92c555978f9ba9d6a326b09f706fd34c
Roman Bulinski <aboandrus@gmail.com>
2019-07-26 17:50:25 +0200

    Refactore code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,6 @@ public class Main {
 
         while(counter<500){
             char next = output.charAt(counter);
-            System.out.println(next);
             if(start != next ){
                 counter++;
             }else if( start == next && output.charAt(1) == output.charAt(counter +1 ) && output.charAt(10) == output.charAt(counter +10 )  ){
@@ -25,32 +24,6 @@ public class Main {
         }
         return -1;
     }
-
-
-
-
-    public static void main(String[] args){
-
-//        BigDecimal one = new BigDecimal(1);
-//        BigDecimal input = new BigDecimal(7);
-//        BigDecimal output = one.divide(input,100, RoundingMode.CEILING);
-//
-//
-//        System.out.println( output.toString() );
-//
-//
-//        BigDecimal a = new BigDecimal("1");
-//        BigDecimal b = new BigDecimal("4");
-//        BigDecimal c = new BigDecimal("0.33");
-//
-//        System.out.println(a.divide(b));
-
-
-
-
-    }
-
-
-
+    
 
 }
